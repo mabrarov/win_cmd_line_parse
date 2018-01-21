457c99a4b8beaea49882f586200cf05dda4f7580
Marat Abrarov <abrarov@gmail.com>
2018-01-22 00:25:01 +0300

    Added C/C++ runtime command line argument parsing test app.

---
+++ cpp_runtime/src/main.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2018 Marat Abrarov (abrarov@gmail.com)
+//
+// Distributed under the Apache License, Version 2.0. (See accompanying
+// file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+//
+
+#if defined(WIN32)
+#include <tchar.h>
+#endif
+
+#include <iostream>
+
+#if defined(WIN32)
+int _tmain(int argc, _TCHAR* argv[])
+#else
+int main(int /*argc*/, char* /*argv*/[])
+#endif
+{
+  for (int i = 0; i < argc; ++i)
+  {
+    std::wcout << argv[i] << std::endl;
+  }
+}

c534f5e635b6b5aabd84391210371b2c2ea3309a
Marat Abrarov <abrarov@gmail.com>
2018-01-22 00:24:16 +0300

    Added copyright headers.

--- cmd/print_args.cmd
+++ cmd/print_args.cmd
@@ -1,3 +1,10 @@
+rem
+rem Copyright (c) 2018 Marat Abrarov (abrarov@gmail.com)
+rem
+rem Distributed under the Apache License, Version 2.0. (See accompanying
+rem file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+rem
+
 @echo off
 
 for %%x in (%*) do (
